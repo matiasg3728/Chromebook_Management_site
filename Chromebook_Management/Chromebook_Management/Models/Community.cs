86b7923661d58bb0d58cf75d57d8c10e1cf8ee55
Matias Garcia <matiasg3728@gmail.com>
2019-03-13 23:24:49 -0500

    Made the Community Model!
    O shid waddup homie :)))))))))))))

---
+++ Chromebook_Management/Chromebook_Management/Models/Community.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chromebook_Management.Models {
+    public class Community
+    {
+        public int CompanyID { get; set; }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Chromebook> Chromebooks { get; set; }
+
+
+    }
+}
